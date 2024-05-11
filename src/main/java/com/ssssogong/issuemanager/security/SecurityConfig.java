3d4b8943aa4d45fd4624d73b229839a17c17e18a
Jaehwan Hyun <jec0101@naver.com>
2024-05-11 19:19:18 +0900

    update : updated security configs 기본적인 설정 추가

--- src/main/java/com/ssssogong/issuemanager/security/SecurityConfig.java
+++ src/main/java/com/ssssogong/issuemanager/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @RequiredArgsConstructor
 @Configuration
@@ -13,8 +14,20 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorizedHttpRequests) -> authorizedHttpRequests
+                .anyRequest().permitAll()
+        );
+
         http.formLogin((auth)->auth.disable()) // form 로그인 방식 해제
+                .httpBasic((auth) -> auth.disable()) // http basic 해제
+                .csrf((auth)->auth.disable()) // csrf 해제
+                .headers((headers) -> headers // 프레임 관련 (h2)
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN
+                        ))
+                )
         ;
+
                 
         return http.build();
     }
