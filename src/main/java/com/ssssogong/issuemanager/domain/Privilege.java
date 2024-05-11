4bc6998d86abfe69704aa89770148b294e3f6c49
Jaehwan Hyun <jec0101@naver.com>
2024-05-11 19:09:40 +0900

    add : added Privilege enum 형으로 권한 나열 권한 변경 생길 시 이 부분 수정

---
+++ src/main/java/com/ssssogong/issuemanager/domain/Privilege.java
@@ -0,0 +1,10 @@
+package com.ssssogong.issuemanager.domain;
+
+public enum Privilege {
+    ISSUE_REPORTABLE,
+    ISSUE_ASSIGNABLE,
+    ISSUE_FIXABLE,
+    ISSUE_RESOLVABLE,
+    ISSUE_CLOSABLE,
+    ISSUE_REOPENABLE
+}
