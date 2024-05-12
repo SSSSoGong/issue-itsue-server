1e3c49959b59cb9e6bf02f60b52ea54bf9bdc99c
hectick <ch.yeondev@gmail.com>
2024-05-12 20:01:06 +0900

    add: added JwtUtil for creating Jwt token jwt 토큰 생성을 위한 기능 구현

---
+++ src/main/java/com/ssssogong/issuemanager/util/JwtUtil.java
@@ -0,0 +1,37 @@
+package com.ssssogong.issuemanager.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    private static final String ACCOUNT_ID = "accountId";
+    private static final int MINUTES_TO_MILLISECONDS = 60 * 1000;
+
+    private final SecretKey secretKey;
+    private final long expirationMinutes;
+
+    public JwtUtil(@Value("${auth.jwt.secret-key}") final String secretKey,
+                   @Value("${auth.jwt.expiration-minutes}") final long expirationMinutes) {
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        this.expirationMinutes = expirationMinutes;
+    }
+
+    public String createToken(final long accountId) { //todo: 어떤 정보를 담을까? admin 여부?
+        Date now = new Date();
+        return Jwts.builder()
+                .claim(ACCOUNT_ID, accountId)
+                .issuedAt(now)
+                .expiration(new Date(now.getTime() + expirationMinutes * MINUTES_TO_MILLISECONDS))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    //todo: 토큰에서 유저 정보 extract
+}
---
+++ src/test/java/com/ssssogong/issuemanager/util/JwtUtilTest.java
@@ -0,0 +1,57 @@
+package com.ssssogong.issuemanager.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import javax.crypto.SecretKey;
+import org.junit.jupiter.api.Test;
+
+class JwtUtilTest {
+
+    private static final String SECRET_KEY_TEXT = "hello-my-name-is-secret-key!!!nice-to-meet-U";
+    private static final SecretKey SECRET_KEY = Keys.hmacShaKeyFor(SECRET_KEY_TEXT.getBytes(StandardCharsets.UTF_8));
+    private static final int EXPIRATION_MINUTES = 30;
+    private static final String ACCOUNT_ID = "accountId";
+    private static final JwtUtil JWT_UTIL = new JwtUtil(SECRET_KEY_TEXT, EXPIRATION_MINUTES);
+
+    @Test
+    void JWT_토큰_생성() {
+        final long accountId = 1L;
+        final String token = JWT_UTIL.createToken(accountId);
+
+        assertThat(token).isNotBlank();
+    }
+
+    @Test
+    void 시크릿_키로_JWT_토큰_검증() {
+        final long accountId = 1L;
+        final String token = JWT_UTIL.createToken(accountId);
+
+        assertDoesNotThrow(
+                () -> Jwts.parser()
+                        .verifyWith(SECRET_KEY)
+                        .build()
+                        .parseSignedClaims(token)
+                        .getPayload()
+        );
+    }
+
+    @Test
+    void 시크릿_키로_JWT_토큰에서_계정_아이디_추출() {
+        final long accountId = 1L;
+        final String token = JWT_UTIL.createToken(accountId);
+
+        final Claims payload = Jwts.parser()
+                .verifyWith(SECRET_KEY)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        final Long extractedAccountId = payload.get(ACCOUNT_ID, Long.class);
+        assertThat(extractedAccountId).isEqualTo(accountId);
+    }
+}
