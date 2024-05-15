4f652f45410912fc162562f60b21d52960780d7e
hectick <ch.yeondev@gmail.com>
2024-05-15 13:16:54 +0900

    update : 회의 사항 반영

--- src/main/java/com/ssssogong/issuemanager/util/AccessAccount.java
+++ src/main/java/com/ssssogong/issuemanager/util/AccessAccount.java
@@ -7,6 +7,7 @@ public class AccessAccount {
 
     private final String accountId;
     private final boolean isAdmin;
+    //todo: role 추가
 
     public AccessAccount(final String accountId, final boolean isAdmin) {
         this.accountId = accountId;
