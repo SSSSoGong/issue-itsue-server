022ee07b45e26b4480f7fccde7f0935556cd8298
JaehwanH <54016683+ja7811@users.noreply.github.com>
2024-05-15 13:07:40 +0900

    feat : Roles & Privileges (#3)
    * add : added security configuration
    초기 설정
    
    * add : added application.yml
    application.properties삭제 및 yml로대체
    
    * add : added Privilege
    enum 형으로 권한 나열
    권한 변경 생길 시 이 부분 수정
    
    * add : added StringCollectionConverter
    Collection<String>을 하나의 String으로 변환
    - SPLIT_CHAR를 기준으로 하나로 묶음
    - Role 엔티티 DB에 저장할 때 사용
    
    * add : added Role
    Privilege를 묶는 엔티티입니다
    allowedPrivilege : 허용된 권한 목록
    - 컬렉션이지만 StringCollectionConverter로 문자열 변환하여 하나의 칼럼에 저장
    
    * update : updated security configs
    기본적인 설정 추가
    
    * add: added specific roles
    dev, pl, tester 추가
    
    * move : changed package
    domain -> domain.role 패키지로 변경
    
    * fix : Role이 상속받는 패키지 변경
    GrantedAuthority -> GrantedAuthoritiesContainer
    여러 Authority를 담으므로 GrantedAuthoritiesContainer로 수정
    + Role 상속받는 클래스 Override 메소드 수정
    
    * fix : Role concrete class로 변경
    getGrantedAuthorities가 중복되어 묶어냄
    생성자에 Privilege 넣어서 임시 Role 만드는 것도 가능
    
    * update : 회의 사항 반영

---
+++ src/main/java/com/ssssogong/issuemanager/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.ssssogong.issuemanager.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorizedHttpRequests) -> authorizedHttpRequests
+                .anyRequest().permitAll()
+        );
+
+        http.formLogin((auth)->auth.disable()) // form 로그인 방식 해제
+                .httpBasic((auth) -> auth.disable()) // http basic 해제
+                .csrf((auth)->auth.disable()) // csrf 해제
+                .headers((headers) -> headers // 프레임 관련 (h2)
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN
+                        ))
+                )
+        ;
+
+                
+        return http.build();
+    }
+}
