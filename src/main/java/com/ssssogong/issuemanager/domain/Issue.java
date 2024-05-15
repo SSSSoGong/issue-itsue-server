285b9330d2ebe111faebf893aedb798764c7f249
신현우 <71479783+Hyun0828@users.noreply.github.com>
2024-05-15 12:50:00 +0900

    Feat/domain (#2)
    * add: Add domain, repo, service, controller
    
    * update: Update yml & Domain
    
    * Update: update Inheritance strategy
    
    * Update: update role domain,service,repository
    
    * Update: Role
    
    * Add: add Issue, Comment, Enum Type
    
    * Add: add domian, repository, JUnit test
    
    * Update : 회의 사항 반영
    
    ---------
    
    Co-authored-by: dlwlsrud815 <dlwlsrud815@gmail.com>

---
+++ src/main/java/com/ssssogong/issuemanager/domain/Issue.java
@@ -0,0 +1,71 @@
+package com.ssssogong.issuemanager.domain;
+
+import com.ssssogong.issuemanager.domain.account.User;
+import com.ssssogong.issuemanager.domain.enumeration.Category;
+import com.ssssogong.issuemanager.domain.enumeration.Priority;
+import com.ssssogong.issuemanager.domain.enumeration.State;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SuperBuilder
+public class Issue extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "issue_id")
+    private Long id;
+
+    private String title;
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    @Enumerated(EnumType.STRING)
+    private State state;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reporter_id")
+    private User reporter;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fixer_id")
+    private User fixer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "assignee_id")
+    private User assignee;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @OneToMany(mappedBy = "issue")
+    @Builder.Default
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "issue")
+    @Builder.Default
+    private List<IssueModification> issueModifications = new ArrayList<>();
+
+    public void setProject(Project project) {
+        if (this.project != null) {
+            this.project.getIssues().remove(this);
+        }
+        this.project = project;
+        project.getIssues().add(this);
+    }
+}
---
+++ src/main/java/com/ssssogong/issuemanager/domain/IssueModification.java
@@ -0,0 +1,47 @@
+package com.ssssogong.issuemanager.domain;
+
+import com.ssssogong.issuemanager.domain.account.User;
+import com.ssssogong.issuemanager.domain.enumeration.State;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SuperBuilder
+public class IssueModification extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "im_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "'from'")
+    private State from;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "'to'")
+    private State to;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "issue_id")
+    private Issue issue;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User modifier;
+
+    public void setIssue(Issue issue) {
+        if (this.issue != null) {
+            this.issue.getIssueModifications().remove(this);
+        }
+        this.issue = issue;
+        issue.getIssueModifications().add(this);
+    }
+
+}
