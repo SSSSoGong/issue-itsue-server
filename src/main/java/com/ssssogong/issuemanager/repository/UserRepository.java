964f6cae9ce556faa2edb4093e61fbd8586b56f0
Jaehwan Hyun <jec0101@naver.com>
2024-05-18 13:54:14 +0900

    update : 메소드 추가 findByAccountId(String) 추가

--- src/main/java/com/ssssogong/issuemanager/repository/UserRepository.java
+++ src/main/java/com/ssssogong/issuemanager/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.ssssogong.issuemanager.repository;
 import com.ssssogong.issuemanager.domain.account.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByAccountId(String accountId);
 }
