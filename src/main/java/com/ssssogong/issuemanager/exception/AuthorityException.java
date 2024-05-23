3deed1c9243a7d8ff3e6b94836615e4106a9e38a
Jaehwan Hyun <jec0101@naver.com>
2024-05-24 01:54:28 +0900

    add : exception에 NoArgsConstructor 추가

--- src/main/java/com/ssssogong/issuemanager/exception/AuthorityException.java
+++ src/main/java/com/ssssogong/issuemanager/exception/AuthorityException.java
@@ -1,5 +1,8 @@
 package com.ssssogong.issuemanager.exception;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class AuthorityException extends RuntimeException{
     public AuthorityException(String message){
         super(message);
--- src/main/java/com/ssssogong/issuemanager/exception/NotFoundException.java
+++ src/main/java/com/ssssogong/issuemanager/exception/NotFoundException.java
@@ -1,5 +1,8 @@
 package com.ssssogong.issuemanager.exception;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
