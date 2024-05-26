c8c6c0511fc536ab81480fa6a441f85741f65d57
Hyun0828 <spdlqj4818@naver.com>
2024-05-26 20:10:40 +0900

    delete: check accoundId

--- src/main/java/com/ssssogong/issuemanager/service/IssueService.java
+++ src/main/java/com/ssssogong/issuemanager/service/IssueService.java
@@ -37,7 +37,6 @@ public class IssueService {
     @Transactional
     public IssueIdResponseDto save(Long projectId, IssueSaveRequestDto issueSaveRequestDto) throws IOException {
 
-        // TODO : security에서 reporter 가져오기
         String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
         User reporter = userRepository.findByAccountId(accountId).orElseThrow(() -> new NotFoundException("해당 user가 없습니다."));
         Project project = projectRepository.findById(projectId).orElseThrow(() -> new NotFoundException("해당 project가 없습니다"));
@@ -60,12 +59,8 @@ public class IssueService {
     @Transactional
     public IssueIdResponseDto update(Long projectId, Long issueId, IssueUpdateRequestDto issueUpdateRequestDto) throws IOException {
         Issue issue = issueRepository.findById(issueId).orElseThrow(() -> new NotFoundException("해당 issue가 없습니다"));
-        if (!issue.getReporter().getAccountId().equals(SecurityContextHolder.getContext().getAuthentication().getName())) // issue에 대한 reporter 당사자가 수정 가능
-            throw new AccessDeniedException("You are not authorized to update this issue");
-        else {
-            issue.update(issueUpdateRequestDto.getTitle(), issueUpdateRequestDto.getDescription(), issueUpdateRequestDto.getPriority());
-            return IssueMapper.toIssueIdResponseDto(issue);  // entity -> dto
-        }
+        issue.update(issueUpdateRequestDto.getTitle(), issueUpdateRequestDto.getDescription(), issueUpdateRequestDto.getPriority());
+        return IssueMapper.toIssueIdResponseDto(issue);  // entity -> dto
     }
 
     //  이슈 삭제
@@ -90,25 +85,10 @@ public class IssueService {
         String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
         User modifier = userRepository.findByAccountId(accountId).orElseThrow(() -> new NotFoundException("해당 user가 없습니다."));
 
-        if (to.equals(State.ASSIGNED)) { // new -> assigned : modifier = PL, assignee = dev
-            if (issueStateUpdateRequestDto.getAssignee() != null && !issueStateUpdateRequestDto.getAssignee().isBlank()) {  // new -> assigned : modifier = PL, assignee = dev
-                User assignee = userRepository.findByAccountId(issueStateUpdateRequestDto.getAssignee()).orElseThrow(() -> new NotFoundException("해당 user가 없습니다"));
-                issue.setAssignee(assignee);
-            }
+        if (issueStateUpdateRequestDto.getAssignee() != null && !issueStateUpdateRequestDto.getAssignee().isBlank()) {  // new -> assigned : modifier = PL, assignee = dev
+            User assignee = userRepository.findByAccountId(issueStateUpdateRequestDto.getAssignee()).orElseThrow(() -> new NotFoundException("해당 user가 없습니다"));
+            issue.setAssignee(assignee);
         }
-        if (to.equals(State.FIXED)) { // assigned -> fixed : modifier = dev, fixer = dev
-            if (issue.getAssignee().getAccountId().equals(modifier.getAccountId())) // issue의 assignee만 FIXED 가능
-                issue.setFixer(modifier);
-            else
-                throw new AccessDeniedException("You are not authorized to update the state for this issue");
-        }
-        if (to.equals(State.RESOLVED) || to.equals(State.REOPENED)) { // fixed -> resolved : modifier = tester, issue의 reporter만 RESOLVED 가능
-            if (!issue.getAssignee().getAccountId().equals(modifier.getAccountId()))
-                throw new AccessDeniedException("You are not authorized to update the state for this issue");
-        }
-        if(to.equals(State.CLOSED))
-            // TODO : issue가 속해 있는 Project의 PL만 가능
-
         issueModificationService.save(issue, from, to, modifier); // issueModification 저장
         return IssueMapper.toIssueIdResponseDto(issue); // entity -> dto
     }
