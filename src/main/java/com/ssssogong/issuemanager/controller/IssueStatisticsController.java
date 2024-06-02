82b8c4a4bb4d638deaffd4a5850fa2f96ed1ad3f
hectick <ch.yeondev@gmail.com>
2024-06-02 19:37:18 +0900

    fix: 프로젝트별로 이슈 통계 조회하도록 수정

--- src/main/java/com/ssssogong/issuemanager/controller/IssueStatisticsController.java
+++ src/main/java/com/ssssogong/issuemanager/controller/IssueStatisticsController.java
@@ -8,42 +8,49 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/issue-statistics")
+@RequestMapping("/projects/{projectId}/issue-statistics")
 public class IssueStatisticsController {
 
     private final IssueStatisticsService issueStatisticsService;
 
     @GetMapping("/daily")
     public ResponseEntity<List<DateStatisticsResponseDto>> getDailyStatistics(
+            @PathVariable(name = "projectId") final Long projectId,
             @RequestParam(name = "period", required = false) final Integer period
     ) {
-        final List<DateStatisticsResponseDto> response = issueStatisticsService.getDailyStatistics(period);
+        final List<DateStatisticsResponseDto> response = issueStatisticsService.getDailyStatistics(projectId, period);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/monthly")
     public ResponseEntity<List<DateStatisticsResponseDto>> getMonthlyStatistics(
+            @PathVariable(name = "projectId") final Long projectId,
             @RequestParam(name = "period", required = false) final Integer period
     ) {
-        final List<DateStatisticsResponseDto> response = issueStatisticsService.getMonthlyStatistics(period);
+        final List<DateStatisticsResponseDto> response = issueStatisticsService.getMonthlyStatistics(projectId, period);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/category")
-    public ResponseEntity<List<CategoryStatisticsResponseDto>> getCategoryStatistics() {
-        List<CategoryStatisticsResponseDto> response = issueStatisticsService.getCategoryStatistics();
+    public ResponseEntity<List<CategoryStatisticsResponseDto>> getCategoryStatistics(
+            @PathVariable(name = "projectId") final Long projectId
+    ) {
+        List<CategoryStatisticsResponseDto> response = issueStatisticsService.getCategoryStatistics(projectId);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/priority")
-    public ResponseEntity<List<PriorityStatisticsResponseDto>> getPriorityStatistics() {
-        List<PriorityStatisticsResponseDto> response = issueStatisticsService.getPriorityStatistics();
+    public ResponseEntity<List<PriorityStatisticsResponseDto>> getPriorityStatistics(
+            @PathVariable(name = "projectId") final Long projectId
+    ) {
+        List<PriorityStatisticsResponseDto> response = issueStatisticsService.getPriorityStatistics(projectId);
         return ResponseEntity.ok(response);
     }
 }
